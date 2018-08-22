a5f70de79a4fdad5f723c1a25a286ae22300ba92
rinda18 <41496540+rinda18@users.noreply.github.com>
2018-08-22 13:43:34 +0700

    Update config.py

--- LINEPY/config.py
+++ LINEPY/config.py
@@ -27,10 +27,10 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[16]
-    APP_VER     = '7.14.0'
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'iPhone OS'
+    SYSTEM_NAME = 'PUY'
     SYSTEM_VER  = '10.12.0'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
